a18bba726458650d500c173dd713ef11f7a7c6fd
azam <azamarif342@gmail.com>
2024-10-31 14:32:54 +0500

    Done

---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color blueColor = Colors.blue;
+
+const Color whiteColor = Colors.white;
+
+const headingTextStyle = TextStyle(fontSize: 30, color: whiteColor);
